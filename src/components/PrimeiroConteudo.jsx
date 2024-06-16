7fb34a8a5345e8dabfb1957e77c1d4e49a25ccc1
Thex <thexnovo@gmail.com>
2024-06-16 00:18:03 -0300

    Alterações no Footer

--- src/components/PrimeiroConteudo.jsx
+++ src/components/PrimeiroConteudo.jsx
@@ -9,7 +9,7 @@ const PrimeiroConteudo = () => {
             <div className='conteiner-1'>
                 <div className="conteiner-titulo">
                     <img src={Risco} alt="" className='risco' />
-                    <h1 className='titulo'> BioCollapse</h1>
+                    <h1 className='titulo'>BioCollapse</h1>
                 </div>
                 <p className='desc'>Em um mundo destruído por um <b>apocalipse zumbi</b> e um <b>colapso ambiental</b>, os sobreviventes precisam não apenas lutar contra os mortos-vivos, mas também reconstruir uma nova sociedade auto-sustentável e com uma governança ética.</p>
             </div>
