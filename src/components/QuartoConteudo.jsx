b0802ab490219d3ab234b83444e3b756736bb453
Thex <thexnovo@gmail.com>
2024-06-24 02:07:00 -0300

    Conteúdo do 'sobre mim' alterado

--- src/components/QuartoConteudo.jsx
+++ src/components/QuartoConteudo.jsx
@@ -7,7 +7,7 @@ const QuartoConeudo = () => {
             <img className='profile-photo' src={Photo} alt="Profile-Photo" />
             <div className='sobre-mim'>
                 <h1 className="sobre-mim-titulo"><b>Olá</b>, meu nome é Théo</h1>
-                <p className='sobre-mim-desc'>Sou desenvolvedor Front-End, e desenvolvi esta página com a biblioteca 'React', tenho 16 anos.</p>
+                <p className='sobre-mim-desc'>Sou desenvolvedor Front-End, e desenvolvi esta página com a biblioteca 'React'. Tenho 16 anos, e amo a programação, e estou em constante evolução.</p>
             </div>
         </div>
     )
